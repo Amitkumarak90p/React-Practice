198f4a30e8de0e35a829b4c1670f845b3557d51d
Amitkumarak90p <theturnorchange@gmail.com>
2025-09-19 13:58:07 +0530

    Starting react Practice from scratch day1

--- src/chai.jsx
+++ src/chai.jsx
@@ -3,6 +3,7 @@ function Chai() {
     <>
       <p>This is hello from the concent! </p>
       <h1>This is hello from chai to drink it .</h1>
+      <h2>Now i am practicing my react skills again</h2>
     </>
   );
 }
