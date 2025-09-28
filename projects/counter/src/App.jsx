9bb185fcc79d62064646fc8521aa6a2657ccdfab
Amitkumarak90p <theturnorchange@gmail.com>
2025-09-28 12:35:55 +0530

    Created a counter project in react using the hooks

---
+++ projects/counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let [counter , setCounter]= useState(10);
+  const addValue = ()=>{
+    setCounter(counter+1);
+    if(counter>=20){
+      setCounter(20);
+    }
+  }
+  const removeValue = ()=>{
+    setCounter(counter-1);
+    if(counter<=0){
+      setCounter(0);
+    }
+  }
+
+
+  return (
+    <>
+      <h1>Coffee aur react</h1>
+      <h2>Count value : {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value </button>
+    </>
+  )
+}
+
+export default App
