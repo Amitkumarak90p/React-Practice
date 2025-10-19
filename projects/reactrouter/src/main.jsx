424b0bb30d201291690ab09adfd43fa5f5e1084f
Amit Kumar <amitkumarak90p@gmail.com>
2025-10-19 13:11:45 +0530

    Adding Routes

--- projects/reactrouter/src/main.jsx
+++ projects/reactrouter/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
+import {StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router";
 import './index.css'
 import App from './App.jsx'
+import Route from './route.jsx';
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Route />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router }/>
   </StrictMode>,
 )
