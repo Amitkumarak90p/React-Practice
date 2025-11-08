0a41f2217699e75f321fa64f201139a71edcec5c
Amit Kumar <amitkumarak90p@gmail.com>
2025-11-08 16:29:12 +0530

    Github api called and data is used

--- projects/reactrouter1/src/main.jsx
+++ projects/reactrouter1/src/main.jsx
@@ -7,6 +7,7 @@ import Home from './components/home/Home.jsx'
 import About from './components/about/About.jsx'
 import Contact from './components/contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 
 const router = createBrowserRouter([
   {
@@ -28,6 +29,10 @@ const router = createBrowserRouter([
       {
         path: "user/:id",
         element: <User/>
+      },
+      {
+        path: "github",
+        element: <Github/>
       }
       
       
